3e44b62ae2542f55a753d8a6fb721a4dfb1f8ec0
vlamxh <vlamxh@proton.me>
2024-04-25 14:34:38 -0700

    modular wm system

---
+++ wm.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+
+   services.xserver = {
+    enable = true;   
+    desktopManager = {
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+    displayManager.defaultSession = "xfce";
+    windowManager.i3.enable = true;
+  };
+
+
+}
